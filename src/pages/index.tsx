8333d2d0d44503d302d7f0548647f046afd33dda
kekelberi <alexei.gaponov@gmail.com>
2022-06-29 17:31:50 +0300

    work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,42 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+export const url = 'https://graphqlzero.almansi.me/api'
 
 const Home: NextPage = () => {
+  const [data, setData] = useState()
+  console.log(data)
+
+  const getQuery = async (query) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    })
+    const data = await res.json()
+    setData(data.data.todos)
+  }
+
+  
+  useEffect(() => {
+    getQuery(`query FilterTodos($options: PageQueryOptions) {
+      todos(options: $options) {
+        data {
+          id
+          title
+          completed
+          user {
+            name
+            address {
+              city
+            }
+          }
+        }
+      }
+    }`)
+  }, [])
+
   return (
     <div>
       <h1 className='text-[42px] text-center pt-10 mb-10'>GraphQl Todos</h1>
